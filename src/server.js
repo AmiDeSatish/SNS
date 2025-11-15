60f2bc4eb50974019dee5362e2e5de2bd8cd67d3
LeRoiDuRERb <tbone_92@outlook.fr>
2025-11-15 11:16:11 +0100

    Add src structure

--- src/server.js
+++ src/server.js
@@ -1,7 +1,23 @@
 import express from "express"
+import path from "path"
+import {fileURLtoPath} from "url"
+ 
+// Get the static files path
+const __filename = fileURLtoPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const StaticFiles = path.join(__dirname,"../public")
 
+ // Important const for the app settings
 const app = express()
 const PORT = 5200
+
+// 1 : Global middleware
+app.use(express.json())
+app.use(express.static(StaticFiles))
+
+app.get("/",(req,res) => {
+  res.sendFile()
+})
 
 app.listen(PORT, () => {
   console.log("App is listening on port 5200")
